e4024e2f3143ad128e034112fa34d6e38e4fc609
haiku-san <e.andaloussi.brahim@gmail.com>
2022-12-28 13:42:06 +0100

    added animation to profile menu dropdown

--- src/components/ProfileButton/index.jsx
+++ src/components/ProfileButton/index.jsx
@@ -7,6 +7,7 @@ import {
     faUser,
     faCircleQuestion,
 } from '@fortawesome/free-regular-svg-icons'
+import { CSSTransition } from 'react-transition-group'
 
 const penIcon = (
     <FontAwesomeIcon
@@ -37,20 +38,109 @@ const questionIcon = (
 
 function ProfileButton() {
     const [isOpen, setIsOpen] = useState(false)
+    const [activeMenu, setActiveMenu] = useState('none')
 
     return (
         <div
             className="profile"
             // onClick={() => setIsOpen(!isOpen)}
-            onMouseEnter={() => setIsOpen(!isOpen)}
-            onMouseLeave={() => setIsOpen(!isOpen)}
+            onMouseEnter={() => {
+                setActiveMenu('profile')
+                setIsOpen(!isOpen)
+            }}
+            onMouseLeave={() => {
+                setActiveMenu('none')
+                setIsOpen(!isOpen)
+            }}
+            // onMouseEnter={() => setIsOpen(!isOpen)}
+            // onMouseLeave={() => setIsOpen(!isOpen)}
         >
             <img
                 src={netflixAvatar}
                 alt="Netflix avatar"
                 className="profile__avatar"
             />
-            {isOpen && (
+            {/* {isOpen && (
+                <CSSTransition
+                    in={activeMenu === 'profile'}
+                    unmountOnExit
+                    timeout={500}
+                    classNames="profile-open"
+                >
+                    <div className="profile__menu">
+                        <ul className="profile__menu-list">
+                            <li className="profile__menu-list-element">
+                                {' '}
+                                <img
+                                    src={netflixAvatar}
+                                    alt="Netflix avatar"
+                                    className="profile__avatar--in-menu"
+                                />
+                                <p>Profile Name</p>
+                            </li>
+                            <li className="profile__menu-list-element">
+                                {' '}
+                                <img
+                                    src={netflixAvatar}
+                                    alt="Netflix avatar"
+                                    className="profile__avatar--in-menu"
+                                />
+                                <p>Profile Name</p>
+                            </li>
+                            <li className="profile__menu-list-element">
+                                {' '}
+                                <img
+                                    src={netflixAvatar}
+                                    alt="Netflix avatar"
+                                    className="profile__avatar--in-menu"
+                                />
+                                <p>Profile Name</p>
+                            </li>
+                        </ul>
+
+                        <div className="profile__menu-divider"></div>
+                        <ul className="profile__menu-list">
+                            <li className="profile__menu-list-element">
+                                <div className="profile__menu-icon-container">
+                                    {penIcon}
+                                </div>
+                                <p>Gérer les profils</p>
+                            </li>
+                            <li className="profile__menu-list-element">
+                                <div className="profile__menu-icon-container">
+                                    {faceIcon}
+                                </div>
+
+                                <p>Transférer un profil</p>
+                            </li>
+                            <li className="profile__menu-list-element">
+                                <div className="profile__menu-icon-container">
+                                    {userIcon}
+                                </div>
+
+                                <p>Compte</p>
+                            </li>
+                            <li className="profile__menu-list-element">
+                                <div className="profile__menu-icon-container">
+                                    {questionIcon}
+                                </div>
+
+                                <p>Centre d'aide</p>
+                            </li>
+                        </ul>
+                        <div className="profile__menu-divider"></div>
+                        <div className="profile__menu-button">
+                            <p>Se déconnecter</p>
+                        </div>
+                    </div>
+                </CSSTransition>
+            )} */}
+            <CSSTransition
+                in={activeMenu === 'profile'}
+                unmountOnExit
+                timeout={400}
+                classNames="profile-open"
+            >
                 <div className="profile__menu">
                     <ul className="profile__menu-list">
                         <li className="profile__menu-list-element">
@@ -117,7 +207,7 @@ function ProfileButton() {
                         <p>Se déconnecter</p>
                     </div>
                 </div>
-            )}
+            </CSSTransition>
         </div>
     )
 }
